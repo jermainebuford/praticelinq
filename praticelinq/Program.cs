69267845b2caddfdccaf14c68d6e680f3b036d84
jermaine buford <bufordjermaine@outlook.com>
2022-06-20 15:30:37 -0400

    first commit

---
+++ praticelinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] videogamenames = {"the legend of zelda", "metal gear solid", "devil may cry","final fantasy","kingdom hearts"};
+
+        videogamenames.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);
+        }
+    }
+
